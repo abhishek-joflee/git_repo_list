3c6ef786a86624aacaf08048b9fcdb378fd1bd08
abhishek-joflee <abhishek.patil@joflee.com>
2022-01-04 17:58:15 +0530

    api loader not shown when repos are done

--- lib/logic/repo_controller.dart
+++ lib/logic/repo_controller.dart
@@ -9,11 +9,20 @@ class RepoController {
   final Box<Repo> repoBox = Hive.box(repoBoxName);
   final Box utilsBox = Hive.box(utilsBoxName);
 
+  bool _canMakeAPICall = true;
+
   /// it fetches repos & add repo object to hive box
   Future<void> fetchRepos() async {
+    if (!_canMakeAPICall) {
+      return;
+    }
     // GET LAST FETCHED PAGE COUNT & GET ALL REPOS
     int pageCount = utilsBox.get(lastFetchedPageName, defaultValue: 0);
     List<Repo> repos = await repoRepository.getRepos(pageCount);
+
+    if (repos.length < 15) {
+      _canMakeAPICall = false;
+    }
 
     // STORE LAST FETCHED PAGE COUNT
     await utilsBox.put(lastFetchedPageName, pageCount + 1);
