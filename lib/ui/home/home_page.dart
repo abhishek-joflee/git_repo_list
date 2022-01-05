26686bbd7270467d4683d76b212f112deb0be340
abhishek-joflee <abhishek.patil@joflee.com>
2022-01-05 09:46:07 +0530

    temp delete button added

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:git_repo_list/data/models/repo.dart';
+import 'package:git_repo_list/utils/my_hive_constants.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'repo_list_view.dart';
 
@@ -15,6 +18,18 @@ class MyHomePage extends StatelessWidget {
           "Jake's Git",
           style: TextStyle(fontWeight: FontWeight.w600),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Box<Repo> repoBox = Hive.box<Repo>(repoBoxName);
+              repoBox.deleteAll(repoBox.keys);
+
+              Box utilsBox = Hive.box(utilsBoxName);
+              utilsBox.deleteAll(utilsBox.keys);
+            },
+            icon: const Icon(Icons.delete),
+          ),
+        ],
       ),
 
       // body
